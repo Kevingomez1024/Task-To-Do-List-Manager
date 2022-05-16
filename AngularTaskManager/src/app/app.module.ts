05e3e539db3f709a36534fdee8a7844f1888b6a4
Kevin Gomez <kevingomez@Kevins-MacBook-Pro.local>
2022-05-16 18:52:21 -0400

    Added description popovers using ngBootstrap as well as sorting for status

--- AngularTaskManager/src/app/app.module.ts
+++ AngularTaskManager/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -23,7 +24,8 @@ import { FormsModule,ReactiveFormsModule } from '@angular/forms';
     AppRoutingModule,
     HttpClientModule,
     FormsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    NgbModule
   ],
   providers: [SharedService],
   bootstrap: [AppComponent]
